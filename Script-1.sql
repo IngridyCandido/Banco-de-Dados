73a84da9448b67b5e6ec0b4ed53acd80a19b5603
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-12-18 15:32:25 -0300

    Add files via upload

---
+++ Script-1.sql
@@ -0,0 +1,129 @@
+create table cliente(
+	cliente_id serial primary key ,
+	loja_id integer references loja (loja_id),
+	nome text,
+	sobrenome text,
+	email text,
+	endereco_id serial,
+	ativo boolean,
+	data_criacao date,
+	ultima_atualizacao date	
+);
+
+create table loja (
+	loja_id serial primary key,
+	endereco_id integer references endereco (endereco_id),
+	ultima_atualizacao date 
+);
+
+create table funcionario (
+	funcionario_id serial primary key,
+	nome text,
+	sobrenome text,
+	endereco_id serial,
+	email text,
+	ativo boolean,
+	loja_id integer references loja (loja_id),
+	gerente boolean,
+	login text,
+	senha text,
+	ultima_atualizacao date,
+	foto bytea
+);
+
+create table pagamento (
+	pagamento_id serial primary key,
+	funcionario_id integer references funcionario (funcionario_id),
+	cliente_id integer references cliente (cliente_id),
+	aluguel_id integer references aluguel (aluguel_id),
+	valor serial,
+	data_pagamento date
+);
+
+create table aluguel (
+	aluguel_id serial primary key,
+	funcionario_id integer references funcionario (funcionario_id),
+	cliente_id integer references cliente (cliente_id),
+	data_aluguel date,
+	data_retorno date,
+	inventario_id serial,
+	ultima_atualizacao date
+);
+
+create table pais (
+	pais_id serial primary key,
+	pais text,
+	ultima_atualizacao date 
+);
+
+create table cidade (
+	cidade_id serial primary key,
+	cidade text,
+	pais_id integer references pais (pais_id),
+	ultima_atualizacao date 
+);
+
+create table endereco (
+	endereco_id serial primary key,
+	endereco text,
+	endereco2 text,
+	bairro text,
+	cidade_id integer references cidade (cidade_id),
+	cep text,
+	telefone text,
+	ultima_atualizacao date 
+);
+
+create table inventario (
+	inventario_id serial primary key,
+	filme_id integer references filme (filme_id),
+	loja_id integer references loja (loja_id),
+	ultima_atualizacao date 
+);
+
+create table filme (
+	filme_id serial primary key,
+	titulo text,
+	descricao text,
+	ano_lancamento date,
+	idioma serial,
+	duracao_aluguel serial,
+	taxa_aluguel serial,
+	tamanho serial, 
+	custo_reposicao serial,
+	taxa serial,
+	ultima_atualizacao date,
+	caracteristicas_especiais text
+);
+
+create table idioma (
+	idioma_id serial primary key,
+	nome text,
+	ultima_atualizacao date 
+);
+
+
+create table ator_filme (
+	filme_id integer references filme (filme_id),
+	ator_id integer references ator (ator_id),
+	ultima_atualizacao date 
+);
+
+create table ator (
+	ator_id serial primary key,
+	nome text,
+	sobrenome text,
+	ultima_atualizacao date 
+);
+
+create table categoria_filme (
+	filme_id integer references filme (filme_id),
+	categoria_id integer references categoria (categoria_id),
+	ultima_atualizacao date 
+);
+
+create table categoria (
+	categoria_id serial primary key,
+	nome text,
+	ultima_atualizacao date 
+);
---
+++ Script.sql
@@ -0,0 +1,46 @@
+create table clima (
+	cidade varchar (80),
+	temp_lo int,
+	temp_hi int,
+	prcp real,
+	data date
+);
+
+insert into clima (cidade, temp_lo, temp_hi, prcp, data) values ('Mossoró', -10,15,94.5, current_timestamp);
+
+select * from clima;
+
+create table Produto (
+	produto_num integer primary key,
+	nome text not null unique,
+	preco numeric not null constraint preco_positivo check (preco >0),
+	preco_desconto numeric constraint preco_desconto_positivo check (preco_desconto >0),
+	constraint desconto_valido check (preco >preco_desconto)
+);
+
+
+insert into produto(produto_num,nome,preco)
+values(1,'oreo',4);
+
+select * from produto;
+
+create table Produto (
+	produto_num integer primary key,
+	nome text,
+	preco numeric
+);
+
+create table Pedido (
+	pedido_num integer primary key,
+	produto_num integer references Produto (produto_num),
+	qtd integer
+);
+
+insert into produto (produto_num,nome,preco)
+values(2,'água',1);
+select*from produto
+
+insert into pedido (pedido_num, produto_num, qtd) 
+values (10,2,2);
+select*from pedido
+
